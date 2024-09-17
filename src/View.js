61efe77c1d85fa2457fb66f85b57d195c695d3b6
IvaтLiVa <litvinovivanvadimovich@gmail.com>
2024-09-17 15:11:29 +0400

    Добавлен файл view.js для обработки отображения данных - Реализован рендеринг списка фидов и отображение ошибок - Добавлена интеграция с библиотекой on-change для отслеживания изменений состояния

--- src/View.js
+++ src/View.js
@@ -1,1 +1,17 @@
+export default function render(state) {
+  // рисуем  лист  
+  const feedList = document.querySelector('.feeds');
+  const ulElement = document.createElement('ul');
+  feedList.innerHTML = ''; // Очищаем старые элементы списка
+  feedList.appendChild(ulElement);
 
+  state.feedList.forEach((element) => {
+    const liElement = document.createElement('li');
+    liElement.textContent = element;
+    ulElement.appendChild(liElement);
+  });
+
+  //  рисуем  ошибку 
+  const feedbackElement = document.querySelector('.feedback');
+  feedbackElement.textContent = state.submitForm.error;
+}
---
+++ src/app.js
@@ -0,0 +1,48 @@
+import * as yup from 'yup';
+import onChange from 'on-change';
+import render from './View.js';
+
+export default function app() {
+  const state = {
+    submitForm: {
+      error: '',
+    },
+    feedList: [],
+  };
+
+  function createFormSchema() {
+    return yup.object().shape({
+      inputValue: yup
+        .string()
+        .trim()
+        .url('Ссылка должна быть валидным URL')
+        .required('Поле не может быть пустым')
+        .notOneOf(state.feedList, 'Этот URL уже существует в списке фидов'),
+    });
+  }
+  const watchState = onChange(state, (path) => {
+    render(state);
+  });
+
+  function handleFunction(inputValue) {
+    const formSchema = createFormSchema();
+
+    formSchema
+      .validate({ inputValue }, { abortEarly: false })
+      .then(() => {
+        watchState.feedList.push(inputValue);
+        watchState.submitForm.error = 'Rss успешно  загружен'; // убираем  ошибку  при успешной  валидации
+      })
+      .catch((error) => {
+        watchState.submitForm.error = error.message; // назначаем ошибку  стейту
+      });
+  }
+
+  const form = document.querySelector('.rss-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const inputValue = document.querySelector('#url-input').value;
+    handleFunction(inputValue);
+    document.querySelector('#url-input').value = ''; // Очистка поля ввода
+  });
+}
