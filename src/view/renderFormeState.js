711da9a0477fd3320718fa3214178bc77a72e4e3
IvaтLiVa <litvinovivanvadimovich@gmail.com>
2024-10-04 07:32:14 +0400

    fix  rendeform2

--- src/view/renderFormeState.js
+++ src/view/renderFormeState.js
@@ -1,83 +1,42 @@
-const renderInvalid = (
-  inputField,
-  feedbackElement,
-  submitButton,
-  i18nextInstance,
-) => {
-  submitButton.disabled = false;
-  inputField.classList.add('is-invalid');
-  feedbackElement.classList.remove('text-success', 'text-warning');
-  feedbackElement.classList.add('text-danger');
-  feedbackElement.textContent = i18nextInstance.t('invalid');
-  inputField.value = '';
-  inputField.focus();
-};
-
-const renderSending = (
-  inputField,
-  feedbackElement,
-  submitButton,
-  i18nextInstance,
-) => {
-  submitButton.disabled = true;
-  inputField.classList.remove('is-invalid');
-  feedbackElement.classList.remove('text-danger', 'text-success');
-  feedbackElement.classList.add('text-warning');
-  feedbackElement.textContent = i18nextInstance.t('sending');
-};
-
-const renderAdded = (
-  inputField,
-  feedbackElement,
-  submitButton,
-  form,
-  i18nextInstance,
-) => {
-  submitButton.disabled = false;
-  inputField.classList.remove('is-invalid');
-  feedbackElement.classList.remove('text-danger', 'text-warning');
-  feedbackElement.classList.add('text-success');
-  feedbackElement.textContent = i18nextInstance.t('success');
-  form.reset();
-  inputField.focus();
-};
-
-export default function renderFormState(state, i18nextInstance, elements) {
+export default (state, i18nextInstance, elements) => {
   const {
     feedbackElement, inputField, submitButton, form,
   } = elements;
 
   const renderState = (status) => {
     switch (status) {
-      case 'invalid':
-        renderInvalid(
-          inputField,
-          feedbackElement,
-          submitButton,
-          i18nextInstance,
-        );
+      case 'invalid': {
+        submitButton.disabled = false;
+        inputField.classList.add('is-invalid');
+        feedbackElement.classList.remove('text-success', 'text-warning');
+        feedbackElement.classList.add('text-danger');
+        feedbackElement.textContent = i18nextInstance.t('invalid');
+        inputField.value = '';
+        inputField.focus();
         break;
-      case 'sending':
-        renderSending(
-          inputField,
-          feedbackElement,
-          submitButton,
-          i18nextInstance,
-        );
+      }
+      case 'sending': {
+        submitButton.disabled = true;
+        inputField.classList.remove('is-invalid');
+        feedbackElement.classList.remove('text-danger', 'text-success');
+        feedbackElement.classList.add('text-warning');
+        feedbackElement.textContent = i18nextInstance.t('sending');
         break;
-      case 'added':
-        renderAdded(
-          inputField,
-          feedbackElement,
-          submitButton,
-          form,
-          i18nextInstance,
-        );
+      }
+      case 'added': {
+        submitButton.disabled = false;
+        inputField.classList.remove('is-invalid');
+        feedbackElement.classList.remove('text-danger', 'text-warning');
+        feedbackElement.classList.add('text-success');
+        feedbackElement.textContent = i18nextInstance.t('success');
+        form.reset();
+        inputField.focus();
         break;
+      }
       default:
         break;
     }
   };
 
   renderState(state.formState);
-}
+};
