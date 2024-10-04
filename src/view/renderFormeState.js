692aafc7724e86909cf9fcf77a8faad2190c78e4
IvaтLiVa <litvinovivanvadimovich@gmail.com>
2024-10-04 08:01:57 +0400

    fix  rendeform3

--- src/view/renderFormeState.js
+++ src/view/renderFormeState.js
@@ -1,42 +1,47 @@
 export default (state, i18nextInstance, elements) => {
-  const {
-    feedbackElement, inputField, submitButton, form,
-  } = elements;
-
-  const renderState = (status) => {
-    switch (status) {
-      case 'invalid': {
-        submitButton.disabled = false;
-        inputField.classList.add('is-invalid');
-        feedbackElement.classList.remove('text-success', 'text-warning');
-        feedbackElement.classList.add('text-danger');
-        feedbackElement.textContent = i18nextInstance.t('invalid');
-        inputField.value = '';
-        inputField.focus();
-        break;
-      }
-      case 'sending': {
-        submitButton.disabled = true;
-        inputField.classList.remove('is-invalid');
-        feedbackElement.classList.remove('text-danger', 'text-success');
-        feedbackElement.classList.add('text-warning');
-        feedbackElement.textContent = i18nextInstance.t('sending');
-        break;
-      }
-      case 'added': {
-        submitButton.disabled = false;
-        inputField.classList.remove('is-invalid');
-        feedbackElement.classList.remove('text-danger', 'text-warning');
-        feedbackElement.classList.add('text-success');
-        feedbackElement.textContent = i18nextInstance.t('success');
-        form.reset();
-        inputField.focus();
-        break;
-      }
-      default:
-        break;
-    }
+	let { feedbackElement, inputField, submitButton, form } = elements;
+	let feedbackClass = '';
+	let feedbackText = '';
+  
+	const setSubmitButtonState = (isDisabled) => {
+	  submitButton.disabled = isDisabled;
+	};
+  
+	const renderState = (status) => {
+	  switch (status) {
+		case 'invalid': {
+		  setSubmitButtonState(false);
+		  inputField.classList.add('is-invalid');
+		  feedbackClass = 'text-danger';
+		  feedbackText = i18nextInstance.t('invalid');
+		  inputField.value = '';
+		  inputField.focus();
+		  break;
+		}
+		case 'sending': {
+		  setSubmitButtonState(true);
+		  inputField.classList.remove('is-invalid');
+		  feedbackClass = 'text-warning';
+		  feedbackText = i18nextInstance.t('sending');
+		  break;
+		}
+		case 'added': {
+		  setSubmitButtonState(false);
+		  inputField.classList.remove('is-invalid');
+		  feedbackClass = 'text-success';
+		  feedbackText = i18nextInstance.t('success');
+		  form.reset();
+		  inputField.focus();
+		  break;
+		}
+		default:
+		  break;
+	  }
+  
+	  // Устанавливаем класс и текст для элемента обратной связи
+	  feedbackElement.className = feedbackClass; // Присваиваем класс напрямую
+	  feedbackElement.textContent = feedbackText;
+	};
+  
+	renderState(state.formState);
   };
-
-  renderState(state.formState);
-};
