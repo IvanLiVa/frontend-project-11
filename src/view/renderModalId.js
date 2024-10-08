db42673f61de51b7a9136c4eebae537c9c5c53b7
IvaтLiVa <litvinovivanvadimovich@gmail.com>
2024-10-08 13:06:16 +0400

    refactor task 4.01

--- src/view/renderModalId.js
+++ src/view/renderModalId.js
@@ -1,9 +1,10 @@
 export default function renderModalId(state, i18nextInstance, elements) {
+  const { modalTitle, modalBody, viewButton } = elements;
   const post = state.posts.find((p) => p.id === state.modalPostId);
   if (post) {
-    elements.modalTitle.textContent = i18nextInstance.t(post.title);
-    elements.modalBody.textContent = i18nextInstance.t(post.description);
-    elements.viewButton.onclick = () => {
+    modalTitle.textContent = i18nextInstance.t(post.title);
+    modalBody.textContent = i18nextInstance.t(post.description);
+    viewButton.onclick = () => {
       const link = document.createElement('a');
       link.href = post.link;
       link.target = '_blank';
